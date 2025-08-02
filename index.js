08c238ed131f471ff28f74ac0b6c274b32a4b84a
Noa <linkhojun@naver.com>
2025-08-03 00:55:27 +0900

    feat/refactor: 성별 지정/ html input으로 입력 변경,클래스로 객체 구현

--- index.js
+++ index.js
@@ -14,29 +14,60 @@ const inputsecondName = document.getElementById("secondName");
 const outputFirstNum = document.getElementById("firstNum");
 const outputSecondNum = document.getElementById("secondNum");
 
+const firstBoy = document.getElementById("first-Boy");
+const firstGirl = document.getElementById("first-Girl");
+
+const secondBoy = document.getElementById("second-Boy");
+const secondGirl = document.getElementById("second-Girl");
+let sex1;
+let sex2;
 //Get Names
-const firstName = window.prompt("Please, Input Fisrt Name");
-const secondName = window.prompt("Please, Input Second Name");
+function buttonClick() {
+  const firstName = inputFirstName.value;
+  if (firstBoy.checked) {
+    sex1 = "Boy";
+  } else if (firstGirl.checked) {
+    sex1 = "Girl";
+  } else {
+    sex1 = "None";
+  }
 
-inputFirstName.textContent = firstName;
-inputsecondName.textContent = secondName;
+  console.log(sex1);
 
-//Return Name's Num
+  const secondName = inputsecondName.value;
+  if (secondBoy.checked) {
+    sex2 = "Boy";
+  } else if (secondGirl.checked) {
+    sex2 = "Girl";
+  } else {
+    sex2 = "None";
+  }
 
-let firstContain = [];
-for (let i = 0; i < firstName.length; i++) {
-  firstContain[i] = splitHangul(firstName.charAt(i));
+  console.log(sex1);
+
+  const Name1 = new Names(firstName, sex1);
+  const Name2 = new Names(secondName, sex2);
+
+  outputFirstNum.textContent = Name1.returnName();
+  outputSecondNum.textContent = Name2.returnName();
 }
 
-let secondContain = [];
-for (let i = 0; i < secondName.length; i++) {
-  secondContain[i] = splitHangul(secondName.charAt(i));
+class Names {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+  //Return Name's Num
+  returnName() {
+    let NameContain = [];
+    for (let i = 0; i < this.name.length; i++) {
+      NameContain[i] = splitHangul(this.name.charAt(i));
+    }
+    return `${NameContain[0]} ${NameContain[1]} ${NameContain[2]}`;
+  }
 }
 
-outputFirstNum.textContent = `${firstContain[0]} ${firstContain[1]} ${firstContain[2]}`;
-outputSecondNum.textContent = `${secondContain[0]} ${secondContain[1]} ${secondContain[2]}`;
-
-//Calculate the Name's Num
+//Method How to Run about Name-test
 function splitHangul(char) {
   const code = char.charCodeAt(0) - 0xac00;
   const cho = Math.floor(code / 28 / 21);
